6be0cc6f6c1a0090094dd59447d01eb5620b31cd
JWALT99 <jwalt31@gmail.com>
2021-08-01 23:23:10 -0400

    Creating server, added forbidden files, mime types, index.html and main.html

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+  m = MODEL: Object where app data is kept
+  v = VIEW: Object that holds references to HTML elements with IDs.
+  c = CONTROLLER: Object with functions (methods) that mediate between the MODEL and the VIEW. In our case, all CONTROLLER funtions are "event
+  handlers".
+  
+  In addition, there is one more object that is not part of the MVC
+  architecture:
+  h = HELPER: Object that holds all functions that are not event handlers
+*/
+////////////////////////////////////
+///////| data and references: |/////
+////////////////////////////////////
+
+  ////////////////////////////////////
+  ////| define helper functions: |////
+  ////////////////////////////////////
+
+  ////////////////////////////////////
+  ////| define handler functions: |///
+  ////////////////////////////////////
+  
+  ////////////////////////////////////
+  /////////| initialization: |////////
+  ////////////////////////////////////
+
+  ////////////////////////////////////
+  ///////| establish listeners: |/////
+  ////////////////////////////////////
