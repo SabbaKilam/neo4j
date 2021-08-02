94f0b2345a9ef73e4c5bffc668bf11ee0edf34fc
JWALT99 <jwalt31@gmail.com>
2021-08-01 23:21:11 -0400

    Creating server, added forbidden files, mime types, index.html and main.html

--- server.js
+++ server.js
@@ -1,34 +1,68 @@
-//https://neo4j.com/developer/javascript/
-//https://neo4j.com/cloud/aura/
-
-const http = require( 'http' );
+const http = require('http');
 const fs = require('fs');
-require( 'dotenv' ).config();
-const {
-  tryNeo,
-  dropAllRelationsAB,
-  dropOneRelationAB,
-  relateAB
-} = require('./dbController.js');
 
 const port = 3000;
-const host = `localhost`;
+const host = 'localhost';
 
-http.createServer((req, res)=>{
+const mimeTypes = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'text/javascript',
+    txt: 'text/plain',
+    ico: 'image/icon',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    json: 'application/json',   
+};
 
-  fs.readFile('./userfiles/TheFamily.pdf', (error, content) => {
-    if ( !error ){
+const prohibitedFiles = [
+    "/server.js",
+    "/package.json",
+    "/",
+    
+];
 
-      res.writeHead( 200, {'Content-Type': 'application/pdf'});
-      res.end(content);
-    }
-    else{
-      res.writeHead( 500, {'Content-Type': 'text/html'});
-      res.end(`<center><h1>Trouble getting info from server</h1></center>`);
-    }
-  });
-})
-.listen(port, host, () => {
-  tryNeo("Abbas");
-  console.log( `server running at http://${host}:${port}` )
+const server = http.createServer( serverResponse );
+server.listen( port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`);
 });
+
+///////| functions |///////
+function serverResponse( req, res ){
+    ////| setting up preliminaries |////
+    const method = req.method;
+    let url = decodeURI(req.url);
+    if ( prohibitedFiles.includes(url) ){ url = '/index.html'}
+    let filename = url.split('/').reverse()[0];
+    filename = decodeURI( filename );
+    const extension = filename.split('.').reverse()[0].toLowerCase();
+    const mimeType = mimeTypes[extension] || 'application/octet-stream';
+    const loadFile = filename == 'filelist';
+
+    /////| calling REST functions |/////
+    if ( method == 'GET' && loadFile ) {
+        getFilelist( req, res )
+    }
+    else if ( method == 'GET' && !loadFile ) {
+        getFile( url, mimeType, res )
+    } else {
+        res.writeHead( 200, {'Content-Type': 'text/html'});
+        res.write("<center><h2>Bad or Malformed Request</h2></center>");
+        res.end();
+    }
+}
+
+//////| helper functions |//////
+
+function getFile( url, mimeType, res ){   
+    fs.readFile( "." + url, ( error, content ) => {
+        if ( !error ){
+            res.writeHead( 200, {'Content-Type': mimeType });
+            res.end( content , 'utf-8');             
+        }
+        else {
+            res.writeHead( 404, {'Content-Type': 'text/plain'} );
+            res.end( `Trouble finding file "${url}": 404 Error.` , 'utf-8');  
+        }        
+    });
+}
